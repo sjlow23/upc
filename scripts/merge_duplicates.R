38a4170d62785b8d8ec778391413d3816fd44f47
Soo Jen Low <soojenl@spartan-login1.hpc.unimelb.edu.au>
2025-02-25 12:30:23 +1100

    Re-introduce identical sequences after aligning

---
+++ scripts/merge_duplicates.R
@@ -0,0 +1,31 @@
+#!/usr/bin/env Rscript
+
+# Load required libraries
+library(tidyr)
+library(dplyr)
+library(data.table)
+
+
+args <- commandArgs(trailingOnly=TRUE)
+
+aln <- fread(args[2], header=FALSE, sep="\t")
+output <- args[3]
+names(aln) <- c("representative", "primer_set", "size", "fwd", "rev", "alignment")
+
+if (file.exists(args[1])) {
+	# if duplicates present
+	lookup <- fread(args[1], header=F, sep="\t")
+	names(lookup) <- c("genome", "representative")
+	lookup <- lookup %>%
+	separate_rows(genome, sep=", ") %>%
+	left_join(aln, by="representative")
+	} else {
+	cat("No duplicates found\n")
+	lookup <- aln %>% 
+		mutate(genome=representative) %>%
+		relocate(genome, .after=representative)	
+	}
+
+fwrite(lookup, output, sep=",", quote=F, row.names=F, col.names=F)
+
+

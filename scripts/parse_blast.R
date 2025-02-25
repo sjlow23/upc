9eb312a094cb5ec0a5ba55c342ca3d0309b587b4
Soo Jen Low <soojenl@spartan-login3.hpc.unimelb.edu.au>
2025-02-25 21:19:48 +1100

    Fix wrong ref sequence

--- scripts/parse_blast.R
+++ scripts/parse_blast.R
@@ -8,22 +8,27 @@ library(seqinr)
 
 args <- commandArgs(trailingOnly=TRUE)
 
+# Convert probe fasta to tabular format
+probes <- readDNAStringSet(args[1])
+probedf <- data.frame(probe = names(probes),
+					ref = as.character(probes))
 
-blast <- fread(args[1], header=F, sep="\t")
-
+blast <- fread(args[2], header=F, sep="\t")
 
 process_df <- function(mydf, outfile) {
 	names(mydf) <- c("probe", "genome", "positions", "primerset", "slen", "length", 
-				  "pident", "nident", "mismatch", "qseq", "sseq")
+				  "pident", "nident", "mismatch", "sseq")
+
 	mydf <- mydf %>%
-		group_by(probe, genome, sseq) %>% 
-		summarize(sequence=toString(unique(qseq))) %>%
+		left_join(probedf, by="probe") %>%
+		group_by(probe, genome, ref) %>% 
+		summarize(sequence=toString(unique(sseq))) %>%
 		distinct() %>%
 		ungroup() %>%
-		mutate(header = paste0(genome, "--", probe, "--", sseq, sep="")) 
+		mutate(header = paste0(genome, "--", probe, "--", ref, sep="")) 
 	
 	write.fasta(sequences=as.list(mydf$sequence), names=mydf$header, file.out=outfile)
 	
 }
 
-process_df(blast, outfile=args[2])
+process_df(blast, outfile=args[3])
